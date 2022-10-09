f8eb5e83443297ee73ac8566e50317da19a70689
Pete <rawlings.pete@gmail.com>
2022-10-10 07:18:07 +1100

    add fetch data and pause log

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,43 @@ import { Box, SimpleGrid } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 import Log from "../Components/Log";
 import Summary from "../Components/Summary";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { StockItemDataArray, StocksData } from "../types";
 
 const Home: NextPage = () => {
+  const [data, setData] = useState<StocksData>([]);
+  const [logData, setLogData] = useState<StocksData>([]);
+  const [logPaused, setLogPaused] = useState(false);
+
+  const fetchData = async () => {
+    return await fetch("https://join.reckon.com/stock-pricing")
+      // return await fetch("https://jsonplaceholder.typicode.com/posts?_limit=3")
+      .then((res) => res.json())
+
+      .catch((e) => {
+        console.log("Response Error", e);
+      });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData().then((newData: StockItemDataArray) => {
+        console.log("newData", newData);
+        const dataWithTimeStamp = {
+          timeStamp: format(new Date(), "MM/dd/yyyy hh:mm:ss"),
+          data: newData,
+        };
+
+        if (!logPaused) {
+          setLogData([dataWithTimeStamp, ...logData] as StocksData);
+        }
+        setData([dataWithTimeStamp, ...data] as StocksData);
+      });
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [data, logPaused, logData]);
   return (
     <Container maxW="1500" maxH="100vh" mt={10} mb={10}>
       <SimpleGrid minH="100vh" columns={[1, 2]} spacing={10}>
